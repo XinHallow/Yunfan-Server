592581f775ebc0a6ee7f3c107d203c87e0e5c739
Xin Hallow <XinHallow@outlook.com>
2025-03-07 21:41:10 +0800

    feat: 新增整数随机器和测试并添加对断言的依赖

---
+++ src/generators/random-int.ts
@@ -0,0 +1,60 @@
+/**
+ * 根据设置生成随机整数数组
+ * @param min 最小值(>0)
+ * @param max 最大值(>0)
+ * @param count 生成个数(>0)
+ * @param exclude 排除的值
+ * @returns 生成出的不包含重复数字的数组
+ */
+export default (
+  min: number,
+  max: number,
+  count: number,
+  exclude: number[]
+): number[] => {
+  // 检查传入的参数
+  if (min > max) {
+    throw new Error("最小值必须大于最大值");
+  }
+  if (isNaN(min) || isNaN(max) || isNaN(count)) {
+    throw new Error("参数必须是数字");
+  }
+  if (count <= 0) {
+    throw new Error("生成个数必须大于0");
+  }
+
+  // 检查传入的参数是否都是整数
+  if (
+    !Number.isInteger(min) ||
+    !Number.isInteger(max) ||
+    !Number.isInteger(count)
+  ) {
+    throw new Error("参数必须是整数");
+  }
+  exclude.forEach((num) => {
+    if (!Number.isInteger(num)) {
+      throw new Error("排除的值必须是整数");
+    }
+  });
+
+  // 生成可用数字列表
+  const availableNumbers = [];
+  for (let i = min; i <= max; i++) {
+    if (!exclude.includes(i)) availableNumbers.push(i);
+  }
+
+  // 检查可用数字数量是否足够
+  if (availableNumbers.length < count) {
+    throw new Error("可用数字数量不足");
+  }
+
+  // 随机数字
+  const randomNumbers: number[] = [];
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+    randomNumbers.push(availableNumbers[randomIndex]);
+    availableNumbers.splice(randomIndex, 1);
+  }
+
+  return randomNumbers;
+};
