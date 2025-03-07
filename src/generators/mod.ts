052cc9384788709f9dbecb3edc1ec8d59e0a5c7f
Xin Hallow <XinHallow@outlook.com>
2025-03-07 22:41:50 +0800

    feat: 定义生成器和处理器接口

--- src/generators/mod.ts
+++ src/generators/mod.ts
@@ -1,5 +1,14 @@
 import randomInt from "./random-int.ts";
 
+interface Generator {
+  randomInt: (
+    min: number,
+    max: number,
+    count: number,
+    exclude: number[]
+  ) => number[];
+}
+
 export default {
   randomInt: randomInt,
-};
+} as Generator;
--- src/handler/mod.ts
+++ src/handler/mod.ts
@@ -1,5 +1,9 @@
 import randomAPI from "./random.ts";
 
+interface Handlers {
+  [key: string]: (req: Request) => Promise<Response>;
+}
+
 export default {
   random: randomAPI,
-};
+} as Handlers;
